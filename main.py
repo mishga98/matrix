e6e66587f7592026ed01f8a427ec802d228f5b7b
mishga98 <mishga98@gmail.com>
2021-02-10 11:53:57 +0700

    Create simple animation
    Add Qtimer and NumPy lib to create simple animation.
    Now we have just dot rotating by matrix multiplying

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPainter, QPen
 from PyQt5.QtWidgets import QApplication, QWidget, QCheckBox
-from math import sqrt
-import time
+import numpy as np          # contains methods for comfortable matrix usage
+import math
 
-class DrawAlgos(QWidget):
+class AnimAlgos(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.transform)
+        self.x = 50
+        self.y = 50
         self.initUI()
 
     def initUI(self):
@@ -15,21 +19,33 @@ class DrawAlgos(QWidget):
         self.setWindowTitle('Matrix')
         self.show()
 
+    def transform(self):
+        phi = math.pi/90
+        rot = np.aCrerray([
+            [math.cos(phi), math.sin(phi)],
+            [-1*math.sin(phi), math.cos(phi)]
+        ])
+        #self.x += 1
+        #self.y += 1
+        [self.x, self.y] = np.array([self.x, self.y]).dot(rot)
+        self.update()
 
     def paintEvent(self, event):
         width = self.size().width()
         height = self.size().height()
         painter = QPainter(self)
         pen = QPen(Qt.black)
+        pen.setWidth(10)
         painter.setPen(pen)
 
-        #painter.drawPoint(x, y)
+        painter.drawPoint(self.x+150, self.y+150)
+        self.timer.start(5)
 
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
-    w = DrawAlgos()
+    w = AnimAlgos()
     w.show()
     sys.exit(app.exec_())
